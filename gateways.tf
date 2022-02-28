1c318e0e0004b8e46f6dc5c99e7fa6169bd4d42c
tejap <btejapramod@gmail.com>
2022-02-28 17:15:36 +0530

    New Changes

--- gateways.tf
+++ gateways.tf
@@ -5,3 +5,17 @@ resource "aws_internet_gateway" "gw" {
     Name = "${var.ENV}-igw"
   }
 }
+
+resource "aws_eip" "public" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.public.id
+  subnet_id     = aws_subnet.public-subnets.*.id
+
+  tags = {
+    Name = "NAT-gw"
+  }
+  depends_on = [aws_internet_gateway.gw]
+}
