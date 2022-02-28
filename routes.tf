df693da1855b1c28bc9f9bb174a60b9672eff1e7
tejap <btejapramod@gmail.com>
2022-02-28 16:20:24 +0530

    New Changes

--- routes.tf
+++ routes.tf
@@ -16,3 +16,9 @@ resource "aws_route" "route-from-default-vpc" {
   destination_cidr_block    = var.VPC_CIDR
   vpc_peering_connection_id = aws_vpc_peering_connection.peer-to-default-vpc.id
 }
+
+resource "aws_route" "route-for-igw" {
+  route_table_id            = aws_route_table.route-table.id
+  destination_cidr_block    = 0.0.0.0/0
+  gateway_id = aws_internet_gateway.gw.id
+}
