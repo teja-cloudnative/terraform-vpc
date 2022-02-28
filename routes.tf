9b697f65a5224345116ea5f649dbe52de123b842
tejap <btejapramod@gmail.com>
2022-02-28 15:51:52 +0530

    New Changes

--- routes.tf
+++ routes.tf
@@ -5,4 +5,8 @@ resource "aws_route_table" "route-table" {
   }
 }
 
-
+resource "aws_route" "route-to-default-vpc" {
+  route_table_id            = aws_route_table.route-table.id
+  destination_cidr_block    = var.DEFAULT_VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer-to-default-vpc.id
+}
--- vars.tf
+++ vars.tf
@@ -4,3 +4,4 @@ variable "SUBNETS" {}
 variable "AZ" {}
 variable "DEFAULT_VPC_ID" {}
 variable "DEFAULT_ROUTE_TABLE_ID" {}
+variable "DEFAULT_VPC_CIDR" {}
